9307e8f32f9da6844226368ada34ab04ccfc2c81
Alexey <a.yasinko@mail.ru>
2022-06-07 23:40:06 +0300

    Refactoring

--- Assets/Scripts/PlatesSpawner.cs
+++ Assets/Scripts/PlatesSpawner.cs
@@ -7,22 +7,24 @@ public class PlatesSpawner : MonoBehaviour
 {
     public static PlatesSpawner Instance;
 
-    public GameObject platePrefab;
-    public GameObject firstPlatesParent;
-    public GameObject secondPlatesParent;
+    [SerializeField] private GameObject platePrefab;
+    [SerializeField] private GameObject firstPlatesParent;
+    [SerializeField] private GameObject secondPlatesParent;
+    [SerializeField] private GameObject currentTopParent;
 
-    public int generalValue = 0;
+    [SerializeField] private int generalValue = 0;
 
-    public float startPlateXPos;
-    public float startPlateZPos;
+    [SerializeField] private float startPlateXPos;
+    [SerializeField] private float startPlateZPos;
+
+    [SerializeField] private bool needSpawnToFirstParent = true;
+
+    [SerializeField] private List<Plate> currentTopPlates = new List<Plate>();
+    [SerializeField] private List<Plate> firstContainerElements = new List<Plate>();
+    [SerializeField] private List<Plate> secondContainerElements = new List<Plate>();
 
     private float lastPlateXPos;
     private float lastPlateZPos;
-
-    public List<Plate> currentTopPlates = new List<Plate>();
-
-    public bool isFirstParentOnTop = true;
-    public bool needSpawnToFirstParent = true;
 
     private void Awake()
     {
@@ -31,38 +33,7 @@ public class PlatesSpawner : MonoBehaviour
 
     void Start()
     {
-        ResetLastSpawnPoints();
-        for (var i = 0; i < 50; i++)
-        {
-            SpawnPlatesFieldByGeneralValue();
-            firstPlatesParent.transform.GetChild(i).GetComponent<BoxCollider>().enabled = true;
-        }
-
-        needSpawnToFirstParent = !needSpawnToFirstParent;
-
-        SpawnNewPlatesField();
-    }
-
-    public void SpawnPlatesFieldByGeneralValue()
-    {
-        GameObject newPlate;
-        Plate plateComponent;
-
-        if (needSpawnToFirstParent)
-        {
-            Vector3 newPlatePosition = new Vector3(lastPlateXPos, 0, lastPlateZPos);
-            newPlate = Instantiate(platePrefab, newPlatePosition, Quaternion.Euler(270, 0, 0), firstPlatesParent.transform);
-        }
-        else
-        {
-            Vector3 newPlatePosition = new Vector3(lastPlateXPos, 0, lastPlateZPos);
-            newPlate = Instantiate(platePrefab, newPlatePosition, Quaternion.Euler(270, 0, 0), secondPlatesParent.transform);
-            newPlate.transform.localPosition += new Vector3(0, -4, 0);
-        }
-        plateComponent = newPlate.GetComponent<Plate>();
-        currentTopPlates.Add(plateComponent);
-        plateComponent.SetNewNonZeroValue(generalValue);
-        CalculateNewPlatePosition();
+        SpawnStartPlates();
     }
 
     public void SpawnNewPlatesField()
@@ -77,48 +48,91 @@ public class PlatesSpawner : MonoBehaviour
         needSpawnToFirstParent = !needSpawnToFirstParent;
     }
 
+    public void SpawnPlatesFieldByGeneralValue()
+    {
+        GameObject newPlate;
+        Plate plateComponent;
+        Vector3 newPlatePosition = new Vector3(lastPlateXPos, 0, lastPlateZPos);
+
+        if (needSpawnToFirstParent)
+        {
+            newPlate = Instantiate(platePrefab, newPlatePosition, Quaternion.Euler(270, 0, 0), firstPlatesParent.transform);
+            plateComponent = newPlate.GetComponent<Plate>();
+            firstContainerElements.Add(plateComponent);
+        }
+        else
+        {
+            newPlate = Instantiate(platePrefab, newPlatePosition, Quaternion.Euler(270, 0, 0), secondPlatesParent.transform);
+            newPlate.transform.localPosition = new Vector3(newPlate.transform.localPosition.x, 0, newPlate.transform.localPosition.z);
+            plateComponent = newPlate.GetComponent<Plate>();
+            secondContainerElements.Add(plateComponent);
+        }
+        plateComponent.SetNewNonZeroValue(generalValue);
+        CalculateNewPlatePosition();
+    }
+
     public void DecreasePlatesCount(Plate plate)
     {
         currentTopPlates.Remove(plate);
-        Destroy(plate);
+        Destroy(plate.gameObject);
+
         if (currentTopPlates.Count == 0)
         {
-            MoveBottomPlatesOnTop();
+            SwapParentsPositions();
+            ReinitializeTopObjects();
+            HandleNewTopPlatesOnSwap();
+            SpawnNewPlatesField();
         }
     }
 
-    private void MoveBottomPlatesOnTop()
+    private void ReassignCurrentTopParent()
     {
-        GameObject currentTopParent;
+        if (currentTopParent == firstPlatesParent)
+            currentTopParent = secondPlatesParent;
+        else
+            currentTopParent = firstPlatesParent;
+    }
+
+    private void HandleNewTopPlatesOnSwap()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            Transform topParentChild = currentTopParent.transform.GetChild(i);
+            topParentChild.DOShakeScale(1, 0.1f, 10, 30, true).SetEase(Ease.InOutBack);
+            topParentChild.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+
+    private void SwapParentsPositions()
+    {
         Vector3 firstParentPosition = firstPlatesParent.transform.position;
         Vector3 secondParentPosition = secondPlatesParent.transform.position;
 
         firstPlatesParent.transform.DOMove(secondParentPosition, 2);
         secondPlatesParent.transform.DOMove(firstParentPosition, 2);
-
-        if (isFirstParentOnTop)
-            currentTopParent = secondPlatesParent;
-        else
-            currentTopParent = firstPlatesParent;
-
-        for (int i = 0; i < 50; i++)
-        {
-            var s = DOTween.Sequence();
-            s.Append((currentTopParent.transform.GetChild(i).DOShakeScale(1, 0.1f, 10, 30, true).SetEase(Ease.InOutBack)));
-            currentTopParent.transform.GetChild(i).GetComponent<BoxCollider>().enabled = true;
-        }
-        isFirstParentOnTop = !isFirstParentOnTop;
-
-        SpawnNewPlatesField();
     }
+
+    private void ReinitializeTopObjects()
+    {
+        if (currentTopParent == firstPlatesParent)
+        {
+            currentTopParent = secondPlatesParent;
+            currentTopPlates = secondContainerElements;
+        }
+        else
+        {
+            currentTopParent = firstPlatesParent;
+            currentTopPlates = firstContainerElements;
+        }
+    }
+
+    // private IEnumerator Spawn
 
     private void ResetLastSpawnPoints()
     {
         lastPlateXPos = startPlateXPos;
         lastPlateZPos = startPlateZPos;
     }
-
-
 
     public void CalculateNewPlatePosition()
     {
@@ -129,5 +143,21 @@ public class PlatesSpawner : MonoBehaviour
             lastPlateZPos += 2.5f;
             lastPlateXPos = startPlateXPos;
         }
+    }
+
+    private void SpawnStartPlates()
+    {
+        ResetLastSpawnPoints();
+        for (var i = 0; i < 50; i++)
+        {
+            SpawnPlatesFieldByGeneralValue();
+            firstPlatesParent.transform.GetChild(i).GetComponent<BoxCollider>().enabled = true;
+        }
+        currentTopPlates = firstContainerElements;
+        needSpawnToFirstParent = !needSpawnToFirstParent;
+
+        SpawnNewPlatesField();
+
+        currentTopParent = firstPlatesParent;
     }
 }
