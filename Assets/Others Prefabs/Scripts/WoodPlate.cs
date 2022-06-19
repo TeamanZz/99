b3e52d0a6356cbe441ac45270d53307b69250274
Jew <dima.com655@gmail.com>
2022-06-19 12:28:33 +0300

    Fix Wood Plate

--- Assets/Others Prefabs/Scripts/WoodPlate.cs
+++ Assets/Others Prefabs/Scripts/WoodPlate.cs
@@ -7,6 +7,7 @@ public class WoodPlate : Plate
     [Header("Wood Settings")]
     public Plate mainPlatePrefab;
     public int saveValue;
+    public bool isActive = false;
 
     public void Start()
     {
@@ -16,12 +17,17 @@ public class WoodPlate : Plate
 
     public void DestroyWoodPlate()
     {
+        if (isActive)
+            return;
+
         Plate newPlate = Instantiate(mainPlatePrefab, transform.position, Quaternion.Euler(270, 0, 0), PlatesSpawner.Instance.currentTopParent.transform);
         newPlate.currentIndex = base.currentIndex;
 
         PlatesSpawner.Instance.currentTopPlates.Add(newPlate);
         newPlate.GetComponent<BoxCollider>().enabled = true;
         newPlate.plateIsActive = true;
+
+        isActive = true;
 
         newPlate.SetNewNonZeroValue(saveValue);
         KillPlate();
