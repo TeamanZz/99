70ebf416815acf894b74b5293fd40f8c0ccd6a56
Jew <dima.com655@gmail.com>
2022-06-22 20:08:36 +0300

    Fix

--- Assets/Others Prefabs/Scripts/ArmorPlate.cs
+++ Assets/Others Prefabs/Scripts/ArmorPlate.cs
@@ -35,6 +35,7 @@ public class ArmorPlate : Plate
     {
         Plate newPlate = Instantiate(platePrefab, transform.position, Quaternion.Euler(270, 0, 0), PlatesSpawner.Instance.currentTopParent.transform);
         newPlate.currentIndex = base.currentIndex;
+
         PlatesSpawner.Instance.currentTopPlates.Add(newPlate);
         newPlate.GetComponent<BoxCollider>().enabled = true;
         newPlate.plateIsActive = true;
